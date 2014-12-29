c5339b51ef0e120431f2fba6d458813e34ac6703
Niklas Gerdt <gerdtni@gmail.com>
2014-12-29 09:02:03 +0200

    system test

---
+++ src/test/java/mom/system/IntegrationTest.java
@@ -0,0 +1,17 @@
+package mom.system;
+
+import static org.junit.Assert.*;
+import mom.eventservice.EventService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IntegrationTest {
+    @Autowired
+    private EventService es;
+
+    @Test
+    public void test() {
+        
+    }
+
+}
